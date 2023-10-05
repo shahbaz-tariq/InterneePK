acc88fb7a498aff89c29b9f91b50943f91a1a747
Shahbaz <shahbaztariqb@gmail.com>
2023-10-05 19:29:54 +0500

    Day1: Fragments, BottomNavigation, SplashScreen, App Icon, Done.

---
+++ app/src/main/java/com/example/interneepk/ui/contact/ContactViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.interneepk.ui.contact
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ContactViewModel : ViewModel() {
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is contact Fragment"
+    }
+    val text: LiveData<String> = _text
+}
